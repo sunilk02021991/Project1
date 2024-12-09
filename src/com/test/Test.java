bab454aec0c20e7c4971caadcd5d68d57a531c2c
SUNIL KUMAR <SUNIL KUMAR@sunil>
2024-12-09 18:45:41 +0530

    second commit

---
+++ src/com/test/Test.java
@@ -0,0 +1,20 @@
+package com.test;
+
+public class Test 
+
+{
+	public static void main(String[] args)
+	{
+		int a=20;
+		
+		int b= 30;
+		
+		int c= a+b;
+		
+		
+		System.out.println(c);
+		
+		
+	}
+
+}
